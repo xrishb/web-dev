6fe5e9868980aa4c1905010537ffbcd49a1e1644
Rishabh Rawat <rishurawatx77@gmail.com>
2024-04-23 21:58:46 +0530

    added generators

--- mini-projects/todo-app/script.js
+++ mini-projects/todo-app/script.js
@@ -1,22 +1,33 @@
 const form = document.querySelector("#myform");
 const todo = document.querySelector("#todo");
+const todoList = document.querySelector("#todoList"); // Reference to the container
 const addTodo = document.querySelector(".add");
 
-let tasks=[];
+let tasks = [];
 
-function addTask() {
+function addTask(event) {
     event.preventDefault();
 
     const taskText = todo.value.trim();
 
-    if(taskText!== '') {
-        // append the value to the end
+    if (taskText !== '') {
         tasks.push(taskText);
-
-        // clear the todo input value
         todo.value = '';
-
+        updateTodoList(); // Call the function to update the UI
+        console.log(tasks);
     }
+}
+
+function updateTodoList() {
+    // Clear previous todo list items
+    todoList.innerHTML = '';
+
+    // Create new todo list items and append them to the container
+    tasks.forEach(task => {
+        const todoItem = document.createElement('div');
+        todoItem.textContent = task;
+        todoList.appendChild(todoItem);
+    });
 }
 
 form.addEventListener('submit', addTask);
