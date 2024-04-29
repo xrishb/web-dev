e7176fa25311c76fe09813dd1ff646ac1d60d7a0
Rishabh Rawat <rishurawatx77@gmail.com>
2024-04-29 20:43:44 +0530

    php basics

---
+++ php/basics.php
@@ -0,0 +1,14 @@
+<?php
+    // create a variable
+    $name = "rishabh";
+    // echo/print the variable
+    echo "$name";
+
+    // variables start with $ and php is typed dynamically, means datatype is defined automatically.
+    $num = 1;
+    $isValid = true;
+    $decimals = 1.342f;
+
+    // arrays
+    $arr = array(1,2,3,4,5);
+?>
