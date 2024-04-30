1aaffecebea22cf4c81be76f9d5153beddbbbb96
Rishabh Rawat <rishurawatx77@gmail.com>
2024-04-30 21:46:27 +0530

    oops in js. kinda lame

---
+++ js/oop.js
@@ -0,0 +1,47 @@
+// object literal
+const person = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    greet: function() {console.log('moshi moshi');}
+}
+
+// constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = function() {
+        console.log('moshi moshi from constructor');
+    }
+}
+
+let john = new Person('john', 25);
+john.greet();
+
+/*
+    Each JavaScript object has a prototype, which is another object that it inherits properties and methods from. You can add properties and methods to an object's prototype to share them among all instances of that object.
+*/
+
+// Adding a method to the Person prototype
+Person.prototype.sayAge = function() {
+    console.log(`I am ${this.age} years old.`);
+};
+
+john.sayAge(); // Output: I am 30 years old.
+
+
+// Parent constructor function
+function Animal(name) {
+    this.name = name;
+}
+
+// Child constructor function
+function Dog(name, breed) {
+    Animal.call(this, name); // Call the parent constructor
+    this.breed = breed;
+}
+
+Dog.prototype = Object.create(Animal.prototype); // Inherit from Animal
+Dog.prototype.constructor = Dog; // Set the constructor property correctly
+
+let dog = new Dog('Buddy', 'Golden Retriever');
